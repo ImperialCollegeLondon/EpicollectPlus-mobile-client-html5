c56437a0c8e1044c9080373cba442b819f356502
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2015-02-05 18:21:31 +0000

    Error callback for DELETE statements

--- www/js/model/database/Delete/EC.Delete.js
+++ www/js/model/database/Delete/EC.Delete.js
@@ -7,26 +7,17 @@
  */
 var EC = EC || {};
 EC.Delete = EC.Delete || {};
-EC.Delete = ( function() {"use strict";
-
-		var query_error_message = "";
+EC.Delete = ( function() {
+		"use strict";
 
 		//callback for a transaction error
-		var txErrorCB = function(the_tx, the_error) {
-
+		var errorCB = function(the_tx, the_error) {
 			console.log(EC.Const.TRANSACTION_ERROR);
-			console.log(EC.Delete.query_error_message);
 			console.log(JSON.stringify(the_error));
 			console.log(JSON.stringify(the_tx));
-
-			EC.Delete.query_error_message = "";
-
 		};
 
 		return {
-
-			txErrorCB : txErrorCB,
-			query_error_message : query_error_message
-
+			errorCB : errorCB
 		};
 	}());
