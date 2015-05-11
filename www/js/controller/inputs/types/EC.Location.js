9af188212b9ba53b3701c4000d81241e71191f44
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2015-05-11 08:17:34 +0100

    More work on Google Maps

--- www/js/controller/inputs/types/EC.Location.js
+++ www/js/controller/inputs/types/EC.Location.js
@@ -5,24 +5,21 @@ EC.InputTypes = EC.InputTypes || {};
 EC.InputTypes = (function (module) {
     'use strict';
 
-
-    var timeout;
+    var self;
 
     module.location = function (the_value, the_input) {
 
-        var self = this;
-        var location = {};
         var span_label = $('span.label');
         var value = the_value;
         var input = the_input;
-        var geolocation_request;
-        var is_first_attempt = true;
-        var timeout;
-        var clear_timeout;
         var set_location_btn = $('div#location div#input-location div#set-location');
         var set_location_result = $('textarea#set-location-result');
         var accuracy_result = $('div#location  div#input-location div.current-accuracy-result');
         var accuracy_tip = $('div#location  div#input-location div.location-accuracy-tip');
+        var map_progress_loader = $('div#location div#map-progress-spinner-loader');
+        var map_canvas = $('div#location div#input-location div#map-canvas');
+
+        self = this;
 
         //update label text
         span_label.text(input.label);
@@ -35,85 +32,69 @@ EC.InputTypes = (function (module) {
         //hide feedback when showing the view the first time
         $(accuracy_result).addClass('not-shown');
         $(accuracy_tip).addClass('not-shown');
+        $(map_canvas).addClass('map-hide');
+        $(map_progress_loader).removeClass('not-shown');
 
         //set previous location value if any
         set_location_result.val(value);
 
+        //get a rough location when the view is first loaded
         EC.Notification.showProgressDialog('Wait', 'Locating...');
-        $.when(self.getCurrentPosition()).then(function () {
+        $.when(EC.DevicePosition.getCurrentPosition()).then(function () {
+
+            $(accuracy_result).removeClass('not-shown');
+            $(accuracy_tip).removeClass('not-shown');
+            $(map_canvas).removeClass('map-hide');
+            $(map_progress_loader).addClass('not-shown');
             EC.Notification.hideProgressDialog();
         }, function (error) {
             EC.Notification.hideProgressDialog();
             EC.Notification.showToast('Could not locate', 'long');
         });
 
-        function _showAcquiredLocation() {
+        //todo strings need to be translated
+        function _showAcquiredLocation(has_got_location) {
 
-            $(accuracy_result).find('span').text(Math.floor(location.accuracy));
             $(accuracy_result).removeClass('not-shown');
             $(accuracy_tip).removeClass('not-shown');
+            $(map_canvas).removeClass('not-shown');
+            $(map_progress_loader).addClass('not-shown');
+
+            if (has_got_location) {
+                $(accuracy_result).find('span').text(Math.floor(EC.DevicePosition.coords.accuracy));
+                set_location_result.val(//
+                    'Latitude: ' + EC.DevicePosition.coords.latitude + ',\n' + //
+                    'Longitude: ' + EC.DevicePosition.coords.longitude + ',\n' + //
+                    'Altitude: ' + EC.DevicePosition.coords.altitude + ',\n' + //
+                    'Accuracy: ' + EC.DevicePosition.coords.accuracy + ',\n' + //
+                    'Altitude Accuracy: ' + EC.DevicePosition.coords.altitude_accuracy + ',\n' + //
+                    'Bearing: ' + EC.DevicePosition.coords.heading + '\n');
+
+                //update marker position
+                EC.DevicePosition.marker.setPosition(new google.maps.LatLng(EC.DevicePosition.coords.latitude, EC.DevicePosition.coords.longitude));
+
+                if (!EC.Utils.isChrome()) {
+                    EC.Notification.showToast(EC.Localise.getTranslation('location_acquired'), 'short');
+                }
+            }
+            else {
+                //set location object to empty values
+                set_location_result.val(//
+                    'Latitude: ,\n' + //
+                    'Longitude: ,\n' + //
+                    'Altitude: ,\n' + //
+                    'Accuracy: ,\n' + //
+                    'Altitude Accuracy: ,\n' + //
+                    'Bearing: \n');
+            }
+
+            set_location_btn.one('vclick', _handleSetLocation);
 
             EC.Notification.hideProgressDialog();
 
-            set_location_result.val(//
-                'Latitude: ' + location.latitude + ',\n' + //
-                'Longitude: ' + location.longitude + ',\n' + //
-                'Altitude: ' + location.altitude + ',\n' + //
-                'Accuracy: ' + location.accuracy + ',\n' + //
-                'Altitude Accuracy: ' + location.altitude_accuracy + ',\n' + //
-                'Bearing: ' + location.heading + '\n');
-
-            //update marker position
-            EC.DevicePosition.marker.setPosition(new google.maps.LatLng(location.latitude, location.longitude));
-
-            if (!EC.Utils.isChrome()) {
-                EC.Notification.showToast(EC.Localise.getTranslation('location_acquired'), 'short');
-            }
-            set_location_btn.one('vclick', _getLocation);
-
         }
 
-        //request position
-        //todo this can be improved firing watchPosition when starting the form with a location so the first location is already accurate enough
-        function requestPosition() {
-
-            console.log('requestPosition called');
-
-            //on first attempt, get a quick and rough location just to get started
-            //We do not use getCurrentPosition as it tends to give back a cached position when is it called, not looking for a new one each time
-            if (is_first_attempt) {
-                geolocation_request = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
-                    maximumAge: 0,
-                    timeout: timeout,
-                    enableHighAccuracy: true
-                });
-            }
-            else {
-
-                /*
-                 on subsequent calls, check position for 3 secs and return.
-                 this will improve cases when watchPositionretunr immediately with the same value, as it might return more than once during the 3 secs period
-                 */
-                clear_timeout = window.setTimeout(function () {
-                        //be safe in case after 3 secs we still do not have a location
-                        window.navigator.geolocation.clearWatch(geolocation_request);
-                        _showAcquiredLocation();
-                        console.log('setTimeout called with location');
-                    },
-                    30000 //stop checking after 30 seconds (value is milliseconds)
-                );
-
-                //get location using watchPosition for more accurate results, It is called automatically when movement is detected,
-                //not only when requesting it. Do thjis when user wants to improve location
-                geolocation_request = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
-                    maximumAge: 0,
-                    timeout: timeout,
-                    enableHighAccuracy: true
-                });
-            }
-        }
-
-        var _getLocation = function () {
+        var _handleSetLocation = function () {
 
             set_location_btn.off('vclick');
 
@@ -132,7 +113,10 @@ EC.InputTypes = (function (module) {
                     }
                 }
 
-                requestPosition();
+
+                $.when(EC.DevicePosition.watchPosition()).then(function (response) {
+                    _showAcquiredLocation(response);
+                });
 
             }, function () {
                 console.log('gps NOT enabled');
@@ -143,120 +127,9 @@ EC.InputTypes = (function (module) {
 
         };
 
-        var onWatchSuccess = function (position) {
-
-            console.log('onWatchSuccess called, accuracy: ' + position.coords.accuracy);
-
-            //get HTML5 geolocation component values replacing null with '' for not available components
-            location.latitude = (position.coords.latitude === null) ? '' : position.coords.latitude;
-            location.longitude = (position.coords.longitude === null) ? '' : position.coords.longitude;
-            location.altitude = (position.coords.altitude === null) ? '' : position.coords.altitude;
-            location.accuracy = (position.coords.accuracy === null) ? '' : position.coords.accuracy;
-            location.altitude_accuracy = (position.coords.altitudeAccuracy === null) ? '' : position.coords.altitudeAccuracy;
-            location.heading = (position.coords.heading === null) ? '' : position.coords.heading;
-
-            window.navigator.geolocation.clearWatch(geolocation_request);
-            _showAcquiredLocation();
-
-        };
-
-        // onError Callback receives a PositionError object
-        var onWatchError = function (error) {
-
-            var empty = '';
-
-            window.navigator.geolocation.clearWatch(geolocation_request);
-
-            EC.Notification.hideProgressDialog();
-
-            console.log(JSON.stringify(error));
-
-            if (error.code === 3) {
-                EC.Notification.showAlert(EC.Localise.getTranslation('error'), error.message + EC.Localise.getTranslation('location_fail'));
-
-            } else if (error.code === 1) {
-
-                if (window.device.platform === EC.Const.IOS) {
-                    EC.Notification.showAlert(EC.Localise.getTranslation('error'), EC.Localise.getTranslation('location_service_fail'));
-                }
-            } else {
-                EC.Notification.showAlert(EC.Localise.getTranslation('error'), EC.Localise.getTranslation('unknow_error'));
-            }
-
-            //set location object to empty values
-            set_location_result.val(//
-                'Latitude: ' + empty + ',\n' + //
-                'Longitude: ' + empty + ',\n' + //
-                'Altitude: ' + empty + ',\n' + //
-                'Accuracy: ' + empty + ',\n' + //
-                'Altitude Accuracy: ' + empty + ',\n' + //
-                'Bearing: ' + empty + '\n');
-            //
-
-            set_location_btn.one('vclick', _getLocation);
-
-        };
-
         //bind set location button
-        set_location_btn.off().one('vclick', _getLocation);
+        set_location_btn.off().one('vclick', _handleSetLocation);
 
-    };
-
-
-    module.watchPosition = function () {
-
-
-    };
-
-    module.getCurrentPosition = function () {
-
-        var deferred = new $.Deferred();
-
-        //attach event handler to resolve when map is loaded
-
-
-        //set unlimited timeout for watch position to avoid timeout error on iOS when the device does not move
-        // see http://goo.gl/tYsBSC, http://goo.gl/jYQhgr, http://goo.gl/8oR1g2
-        if (window.device) {
-            timeout = (window.device.platform === EC.Const.IOS) ? Infinity : 30000;
-        }
-
-        function onSuccess(position) {
-
-            var current_position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            var mapOptions = {
-                center: {lat: position.coords.latitude, lng: position.coords.longitude},
-                zoom: 20,
-                disableDefaultUI: true
-            };
-
-
-            EC.DevicePosition.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-            //add current user position
-            EC.DevicePosition.marker = new google.maps.Marker({
-                position: current_position,
-                map: EC.DevicePosition.map,
-                draggable: true
-            });
-
-            google.maps.event.addListenerOnce(EC.DevicePosition.map, 'tilesloaded', function () {
-                // Visible tiles loaded!
-                deferred.resolve();
-            });
-        }
-
-        function onError(error) {
-            console.log(error);
-            deferred.reject();
-        }
-
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-            maximumAge: 0,
-            timeout: timeout,
-            enableHighAccuracy: true
-        });
-
-        return deferred.promise();
     };
 
     return module;
--- www/js/utils/EC.DevicePosition.js
+++ www/js/utils/EC.DevicePosition.js
@@ -7,8 +7,143 @@ EC.DevicePosition = (function () {
     return {
 
         map: {},
-        marker: {}
+        marker: {},
+        coords: {},
+        timeout: 30000,
+        is_first_attempt: true,
+        watchTimeout: function () {
+            //set unlimited timeout for watch position to avoid timeout error on iOS when the device does not move
+            // see http://goo.gl/tYsBSC, http://goo.gl/jYQhgr, http://goo.gl/8oR1g2
+            return (window.device.platform === EC.Const.IOS) ? Infinity : 30000;
+        },
+        getCurrentPosition: function () {
 
+            var deferred = new $.Deferred();
+            var self = this;
+
+            //attach event handler to resolve when map is loaded
+            function onSuccess(position) {
+
+                var current_position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                var mapOptions = {
+                    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                    zoom: 20,
+                    disableDefaultUI: true
+                };
+
+
+                EC.DevicePosition.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                //add current user position
+                EC.DevicePosition.marker = new google.maps.Marker({
+                    position: current_position,
+                    map: self.map,
+                    draggable: true
+                });
+
+                //let's use 'idle' event and a 2 secs timeout, to play it safe
+                // 'tilesloaded' could not be fire if there are network problems
+                google.maps.event.addListenerOnce(EC.DevicePosition.map, 'idle', function () {
+                    window.setTimeout(function () {
+                        deferred.resolve();
+                    }, 2000);
+                });
+            }
+
+            function onError(error) {
+                console.log(error);
+                deferred.reject();
+            }
+
+            navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+                maximumAge: 0,
+                timeout: self.timeout,
+                enableHighAccuracy: true
+            });
+
+            return deferred.promise();
+        },
+        watchPosition: function () {
+
+            var deferred = new $.Deferred();
+            var geolocation_request;
+            var timeout = EC.DevicePosition.watchTimeout();
+
+
+            function onWatchSuccess(position) {
+
+                console.log('onWatchSuccess called, accuracy: ' + position.coords.accuracy);
+
+                //get HTML5 geolocation coords values replacing null with '' for not available values
+                EC.DevicePosition.coords.latitude = (position.coords.latitude === null) ? '' : position.coords.latitude;
+                EC.DevicePosition.coords.longitude = (position.coords.longitude === null) ? '' : position.coords.longitude;
+                EC.DevicePosition.coords.altitude = (position.coords.altitude === null) ? '' : position.coords.altitude;
+                EC.DevicePosition.coords.accuracy = (position.coords.accuracy === null) ? '' : position.coords.accuracy;
+                EC.DevicePosition.coords.altitude_accuracy = (position.coords.altitudeAccuracy === null) ? '' : position.coords.altitudeAccuracy;
+                EC.DevicePosition.coords.heading = (position.coords.heading === null) ? '' : position.coords.heading;
+
+                //clear the current watch
+                window.navigator.geolocation.clearWatch(geolocation_request);
+
+                deferred.resolve(true);
+            }
+
+            //onError Callback
+            function onWatchError(error) {
+
+                console.log(error);
+                window.navigator.geolocation.clearWatch(geolocation_request);
+
+                switch (error.code) {
+                    case 1:
+                        if (window.device.platform === EC.Const.IOS) {
+                            EC.Notification.showAlert(EC.Localise.getTranslation('error'), EC.Localise.getTranslation('location_service_fail'));
+                        }
+                        break;
+                    case 3:
+                        EC.Notification.showAlert(EC.Localise.getTranslation('error'), error.message + EC.Localise.getTranslation('location_fail'));
+                        break;
+                    default :
+                        EC.Notification.showAlert(EC.Localise.getTranslation('error'), EC.Localise.getTranslation('unknow_error'));
+                }
+                deferred.resolve(false);
+            }
+
+            //on first attempt, get a quick and rough location just to get started
+            //We do not use getCurrentPosition as it tends to give back a cached position when is it called, not looking for a new one each time
+            if (self.is_first_attempt) {
+                geolocation_request = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
+                    maximumAge: 0,
+                    timeout: timeout,
+                    enableHighAccuracy: true
+                });
+                self.is_first_attempt = false;
+            }
+            else {
+
+                /*
+                 on subsequent calls, check position for 3 secs and return.
+                 this will improve cases when watchPositionretunr immediately with the same value, as it might return more than once during the 3 secs period
+                 */
+                window.setTimeout(function () {
+
+                    //be safe in case after 3 secs we still do not have a location, and resolve to show old values
+                    window.navigator.geolocation.clearWatch(geolocation_request);
+                    deferred.resolve(true);
+                    console.log('setTimeout called without location');
+                }, 30000);
+
+                //stop checking after 30 seconds (value is milliseconds));
+
+                //get location using watchPosition for more accurate results, It is called automatically when movement is detected,
+                //not only when requesting it. Do thjis when user wants to improve location
+                geolocation_request = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
+                    maximumAge: 0,
+                    timeout: timeout,
+                    enableHighAccuracy: true
+                });
+            }
+
+            return deferred.promise();
+        }
     };
-
 }());
